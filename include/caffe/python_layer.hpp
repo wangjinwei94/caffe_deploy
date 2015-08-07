shallowyuan/caffe

PythonLayer takes parameters by string

include/caffe/python_layer.hpp
@@ -35,6 +35,8 @@ class PythonLayer : public Layer<Dtype> {
   virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
     try {
+      self_.attr("param_str_") = bp::str(
+        this->layer_param_.python_param().param_str());
       self_.attr("setup")(bottom, top);
     } catch (bp::error_already_set) {
       PYTHON_LAYER_ERROR();

wangjinwei94/caffe_deploy

include/caffe/python_layer.hpp
@@ -18,6 +18,8 @@ class PythonLayer : public Layer<Dtype> {
 
   virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
+    self_.attr("param_str") = bp::str(
+        this->layer_param_.python_param().param_str());
     self_.attr("setup")(bottom, top);
   }
   virtual void Reshape(const vector<Blob<Dtype>*>& bottom,