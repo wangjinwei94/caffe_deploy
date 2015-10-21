BVLC/caffe

Added batch normalization layer with test and examples

include/caffe/common_layers.hpp
@@ -70,7 +70,55 @@ class ArgMaxLayer : public Layer<Dtype> {
   size_t top_k_;
 };
 
-/**
+  /**
+  * @brief Batch Normalization per-channel with scale & shift linear transform.
+  *
+  */
+  template <typename Dtype>
+  class BNLayer : public Layer<Dtype> {
+   public:
+    explicit BNLayer(const LayerParameter& param)
+        : Layer<Dtype>(param) {}
+    virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+        const vector<Blob<Dtype>*>& top);
+
+    virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+        const vector<Blob<Dtype>*>& top);
+
+    virtual inline const char* type() const { return "BN"; }
+    virtual inline int ExactNumBottomBlobs() const { return 1; }
+    virtual inline int ExactNumTopBlobs() const { return 1; }
+
+   protected:
+    virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+        const vector<Blob<Dtype>*>& top);
+    virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+        const vector<Blob<Dtype>*>& top);
+    virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+        const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+        const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+    // spatial mean & variance
+    Blob<Dtype> spatial_mean_, spatial_variance_;
+    // batch mean & variance
+    Blob<Dtype> batch_mean_, batch_variance_;
+    // buffer blob
+    Blob<Dtype> buffer_blob_;
+
+    Blob<Dtype> x_norm_;
+    // x_sum_multiplier is used to carry out sum using BLAS
+    Blob<Dtype> spatial_sum_multiplier_, batch_sum_multiplier_;
+
+    // dimension
+    int N_;
+    int C_;
+    int H_;
+    int W_;
+    // eps
+    Dtype var_eps_;
+  };
+  /**
  * @brief Takes at least two Blob%s and concatenates them along either the num
  *        or channel dimension, outputting the result.
  */

wangjinwei94/caffe_deploy

include/caffe/common_layers.hpp
@@ -79,6 +79,55 @@ class ArgMaxLayer : public Layer<Dtype> {
 };
 
 /**
+* @brief Batch Normalization per-channel with scale & shift linear transform.
+*
+*/
+template <typename Dtype>
+class BatchNormLayer : public Layer<Dtype> {
+ public:
+  explicit BatchNormLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  virtual inline const char* type() const { return "BN"; }
+  virtual inline int ExactNumBottomBlobs() const { return 1; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+  // spatial mean & variance
+  Blob<Dtype> spatial_mean_, spatial_variance_;
+  // batch mean & variance
+  Blob<Dtype> batch_mean_, batch_variance_;
+  // buffer blob
+  Blob<Dtype> buffer_blob_;
+
+  Blob<Dtype> x_norm_;
+  // x_sum_multiplier is used to carry out sum using BLAS
+  Blob<Dtype> spatial_sum_multiplier_, batch_sum_multiplier_;
+
+  // dimension
+  int N_;
+  int C_;
+  int H_;
+  int W_;
+  // eps
+  Dtype var_eps_;
+};
+
+/**
  * @brief Index into the input blob along its first axis.
  *
  * This layer can be used to select, reorder, and even replicate examples in a
@@ -145,7 +194,6 @@ class BatchReindexLayer : public Layer<Dtype> {
   void check_batch_reindex(int initial_num, int final_num,
                            const Dtype* ridx_data);
 };
-
 
 /**
  * @brief Takes at least two Blob%s and concatenates them along either the num