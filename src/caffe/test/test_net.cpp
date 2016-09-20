89884858c9ec73821e62ff78bc36cc51f37f525a
wangjinwei <wjw_8090711@163.com>
2016-09-20 22:41:11 +0800

    for windows UT

--- src/caffe/test/test_net.cpp
+++ src/caffe/test/test_net.cpp
@@ -17,30 +17,12 @@
 
 namespace caffe {
 
+static const int max_path=280;
 static inline void MakeTempFilename(string* temp_filename) {
-  temp_filename->clear();
-  *temp_filename = "/tmp/caffe_test.XXXXXX";
-  char* temp_filename_cstr = new char[temp_filename->size() + 1];
-  // NOLINT_NEXT_LINE(runtime/printf)
-  strcpy(temp_filename_cstr, temp_filename->c_str());
-  int fd = mkstemp(temp_filename_cstr);
-  CHECK_GE(fd, 0) << "Failed to open a temporary file at: " << *temp_filename;
-  close(fd);
-  *temp_filename = temp_filename_cstr;
-  delete[] temp_filename_cstr;
-}
-
-static inline void MakeTempDir(string* temp_dirname) {
-  temp_dirname->clear();
-  *temp_dirname = "/tmp/caffe_test.XXXXXX";
-  char* temp_dirname_cstr = new char[temp_dirname->size() + 1];
-  // NOLINT_NEXT_LINE(runtime/printf)
-  strcpy(temp_dirname_cstr, temp_dirname->c_str());
-  char* mkdtemp_result = mkdtemp(temp_dirname_cstr);
-  CHECK(mkdtemp_result != NULL)
-      << "Failed to create a temporary directory at: " << *temp_dirname;
-  *temp_dirname = temp_dirname_cstr;
-  delete[] temp_dirname_cstr;
+  int rand_name=caffe_rng_rand()%100000000;
+  char buf[max_path];
+  sprintf(buf, "%0d", rand_name);
+  *temp_filename=string(buf);
 }
 
 template <typename TypeParam>
