e309642a0f12f0574c0eaf016fb8ebe3fe5060e9
wangjinwei <wjw_8090711@163.com>
2017-03-05 03:49:57 +0800

    update CMake, use compiled protoc

--- update_proto.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd src
-../submodules/protobuf/build_linux_x86_64/bin/protoc caffe/proto/caffe.proto --cpp_out=.
-mkdir -p ../include/caffe/proto
-mv caffe/proto/caffe.pb.h ../include/caffe/proto
-cd ..
