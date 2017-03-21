cec068ea1561c0cb4a7ce26c208858cb2eb487c8
wangjinwei <wjw_8090711@163.com>
2017-03-21 18:11:30 +0800

    refactor glog_deploy

--- include/glog_deploy.hpp
+++ include/glog_deploy.hpp
@@ -16,27 +16,35 @@
 #if defined(__linux__)
 #include <unistd.h>
 #include <sys/time.h>
-static inline int gettimeus(void) {
+static inline int __GetTimeUS(void) throw() {
 	timeval time;
 	gettimeofday(&time, nullptr);
 	return time.tv_usec;
 }
 #else  // defined(__linux__)
-static inline int getpid(void) {
+static inline int getpid(void) throw() {
 	return 0;
 }
-static inline int gettimeus(void) {
+static inline int __GetTimeUS(void) throw() {
 	return 0;
 }
 #endif  // defined(__linux__)
 
+static std::string __TimeString(void) {
+	std::time_t time=std::time(nullptr);
+	const std::tm& time_local=*std::localtime(&time);
+	static const size_t buf_len=21;
+	char buf[buf_len];
+	snprintf(buf, buf_len, "%02d%02d %02d:%02d:%02d.%06d", time_local.tm_mon+1, time_local.tm_mday, time_local.tm_hour,
+			time_local.tm_min, time_local.tm_sec, __GetTimeUS());
+	return std::string(buf);
+}
+
 #if defined(__linux__) && defined(__GNUC__)
 #include <execinfo.h>
 #include <cxxabi.h>
-static inline std::string __Demangle(const std::string& name) {
-	size_t size;
-	int status;
-	char* res=abi::__cxa_demangle(&(name[0]), nullptr, &size, &status);
+static inline std::string __Demangle(const std::string& name) throw() {
+	char* res=abi::__cxa_demangle(&(name[0]), nullptr, nullptr, nullptr);
 	if(res==nullptr) {
 		return name;
 	}
@@ -46,15 +54,42 @@ static inline std::string __Demangle(const std::string& name) {
 		return res_str;
 	}
 }
-#define GLOG_DEPLOY_HAS_BACKTRACE
+static std::string __StackTrace(void) {
+	std::string s;
+	s.reserve(1024);
+	static const int max_trace_num=20;
+	void *buffer[max_trace_num];
+	int trace_num=backtrace(buffer, max_trace_num);
+	char **trace_string=backtrace_symbols(buffer, trace_num);
+	s.append("Call stack trace:\n");
+	for(int i=0; i<trace_num; i++) {
+		std::string raw_name(trace_string[i]);
+		int start=raw_name.find_last_of('(');
+		int end=raw_name.find_last_of('+');
+		if(start!=std::string::npos && end!=std::string::npos) {
+			s.append(raw_name.substr(0, start+1)).append("\n");
+			s.append(__Demangle(raw_name.substr(start+1, end-start-1))).append("\n");
+			s.append(raw_name.substr(end, std::string::npos)).append("\n");
+		}
+		else {
+			s.append(raw_name).append("\n");
+		}
+	}
+	free(trace_string);
+	return s;
+}
+#else
+static std::string __StackTrace(void) {
+	return "Call stack trace is not available.\n";
+}
 #endif  // defined(__linux__) && defined(__GNUC__)
 
 class __GlogDeoloyNullStream: public std::ostream {
 public:
-	inline __GlogDeoloyNullStream(): std::ostream(nullptr) {
+	__GlogDeoloyNullStream() throw(): std::ostream(nullptr) {
 	}
 	template <typename T>
-	inline __GlogDeoloyNullStream& operator<<(T const & x) {
+	inline __GlogDeoloyNullStream& operator<<(T const & x) throw() {
 		return *this;
 	}
 };
@@ -62,7 +97,7 @@ static __GlogDeoloyNullStream __glog_deploy_null_stream;
 
 class __GlogDeployLogMessage {
 public:
-	inline __GlogDeployLogMessage(const std::string& level, const std::string& file, const int line):
+	inline __GlogDeployLogMessage(const std::string& level, const std::string& file, const int line) throw():
 			level_(level), line_(line) {
 		if(level=="FATAL" || enable) {
 			file_=file.substr(file.find_last_of('/')+1, std::string::npos);
@@ -72,62 +107,30 @@ public:
 			stream_=&__glog_deploy_null_stream;
 		}
 	}
-	inline ~__GlogDeployLogMessage() throw() {
-		if(!enable && level_!="FATAL") {
-			return;
-		}
-
-		Stream() << std::endl;
-		if(level_=="FATAL") {
-#ifdef GLOG_DEPLOY_HAS_BACKTRACE
-			const int max_trace_num=20;
-			void *buffer[max_trace_num];
-			int trace_num=backtrace(buffer, max_trace_num);
-			char **trace_string=backtrace_symbols(buffer, trace_num);
-			Stream() << "Call stack trace:" << std::endl;
-			for(int i=0; i<trace_num; i++) {
-				std::string raw_name(trace_string[i]);
-				int start=raw_name.find_last_of('(');
-				int end=raw_name.find_last_of('+');
-				if(start!=std::string::npos && end!=std::string::npos) {
-					Stream() << raw_name.substr(0, start+1);
-					Stream() << __Demangle(raw_name.substr(start+1, end-start-1));
-					Stream() << raw_name.substr(end, std::string::npos) << std::endl;
-				}
-				else {
-					Stream() << raw_name << std::endl;
-				}
+	inline ~__GlogDeployLogMessage() throw(std::runtime_error) {
+		if(enable || level_=="FATAL") {
+			std::string info;
+			info.reserve(1024);
+			info.append(1, level_[0]).append(__TimeString()).append(" ").append(std::to_string(getpid()));
+			info.append(" ").append(file_).append(":").append(std::to_string(line_)).append("]\t");
+			info.append(buffer_.str()).append("\n");
+			if(level_=="FATAL") {
+				info.append(__StackTrace());
 			}
-			free(trace_string);
-#else  // GLOG_DEPLOY_HAS_BACKTRACE
-			Stream() <<"Call stack trace is not available." << std::endl;
-#endif  // GLOG_DEPLOY_HAS_BACKTRACE
-		}
-		std::string info=InfoString();
-		if(enable) {
-			std::cerr << info << buffer_.str();
-		}
-		if(level_=="FATAL") {
-			throw std::runtime_error(info+buffer_.str());
+			if(enable) {
+				std::cerr << info;
+			}
+			if(level_=="FATAL") {
+				throw std::runtime_error(info);
+			}
 		}
 	}
-	inline std::ostream& Stream(void) {
+	inline std::ostream& Stream(void) throw() {
 		return *stream_;
 	}
 	static bool enable;
 
 private:
-	inline std::string InfoString(void) {
-		std::time_t time=std::time(nullptr);
-		const std::tm& time_local=*std::localtime(&time);
-		static const size_t buf_len=21;
-		char buf[buf_len];
-		snprintf(buf, buf_len, "%02d%02d %02d:%02d:%02d.%06d", time_local.tm_mon+1, time_local.tm_mday, time_local.tm_hour,
-				time_local.tm_min, time_local.tm_sec, gettimeus());
-		std::ostringstream ss;
-		ss << level_[0] << buf << " " << getpid() << " " << file_ << ":" << line_ << "]\t";
-		return ss.str();
-	}
 	std::string level_;
 	std::string file_;
 	int line_;
