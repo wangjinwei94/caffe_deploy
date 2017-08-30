f2ad2a411d0f1f38988c2163588240e632e6969e
wangjinwei <wjw_8090711@163.com>
2017-08-31 03:02:40 +0800

    use noexcept to replace throw

--- include/glog_deploy.hpp
+++ include/glog_deploy.hpp
@@ -18,16 +18,16 @@
 #if defined(__linux__)
 #include <unistd.h>
 #include <sys/time.h>
-static inline int __GetTimeUS(void) throw() {
+static inline int __GetTimeUS(void) noexcept(true) {
 	timeval time;
 	gettimeofday(&time, nullptr);
 	return time.tv_usec;
 }
 #else  // defined(__linux__)
-static inline int getpid(void) throw() {
+static inline int getpid(void) noexcept(true) {
 	return 0;
 }
-static inline int __GetTimeUS(void) throw() {
+static inline int __GetTimeUS(void) noexcept(true) {
 	return 0;
 }
 #endif  // defined(__linux__)
@@ -45,7 +45,7 @@ static std::string __TimeString(void) {
 #if defined(__linux__) && defined(__GNUC__) && !defined(__ANDROID__)
 #include <execinfo.h>
 #include <cxxabi.h>
-static inline std::string __Demangle(const std::string& name) throw() {
+static inline std::string __Demangle(const std::string& name) noexcept(true) {
 	char* res=abi::__cxa_demangle(&(name[0]), nullptr, nullptr, nullptr);
 	if(res==nullptr) {
 		return name;
@@ -88,10 +88,10 @@ static std::string __StackTrace(void) {
 
 class __GlogDeoloyNullStream: public std::ostream {
 public:
-	__GlogDeoloyNullStream() throw(): std::ostream(nullptr) {
+	__GlogDeoloyNullStream() noexcept(true): std::ostream(nullptr) {
 	}
 	template <typename T>
-	inline __GlogDeoloyNullStream& operator<<(T const & x) throw() {
+	inline __GlogDeoloyNullStream& operator<<(T const & x) noexcept(true) {
 		return *this;
 	}
 };
@@ -99,7 +99,7 @@ static __GlogDeoloyNullStream __glog_deploy_null_stream;
 
 class __GlogDeployLogMessage {
 public:
-	inline __GlogDeployLogMessage(const std::string& level, const std::string& file, const int line) throw():
+	inline __GlogDeployLogMessage(const std::string& level, const std::string& file, const int line) noexcept(true):
 			level_(level), line_(line) {
 		if(level=="FATAL" || enable) {
 			file_=file.substr(file.find_last_of('/')+1, std::string::npos);
@@ -109,7 +109,8 @@ public:
 			stream_=&__glog_deploy_null_stream;
 		}
 	}
-	inline ~__GlogDeployLogMessage() throw(std::runtime_error) {
+	// TODO: Jinwei: throw here is dangerous
+	inline ~__GlogDeployLogMessage() noexcept(false) {
 		if(enable || level_=="FATAL") {
 			std::string info;
 			info.reserve(1024);
@@ -127,7 +128,7 @@ public:
 			}
 		}
 	}
-	inline std::ostream& Stream(void) throw() {
+	inline std::ostream& Stream(void) noexcept(true) {
 		return *stream_;
 	}
 	static bool enable;
